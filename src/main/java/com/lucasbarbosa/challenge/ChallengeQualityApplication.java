db04d26b6df8045441974cd7923849ac2a981f66
Lucas Barbosa <lucasbarbosagomes0@gmail.com>
2018-12-22 18:54:06 -0200

    Criando o profile de teste

--- src/main/java/com/lucasbarbosa/challenge/ChallengeQualityApplication.java
+++ src/main/java/com/lucasbarbosa/challenge/ChallengeQualityApplication.java
@@ -1,20 +1,11 @@
 package com.lucasbarbosa.challenge;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.lucasbarbosa.challenge.domain.Foto;
-import com.lucasbarbosa.challenge.repositories.FotoRepository;
-
 @SpringBootApplication
 public class ChallengeQualityApplication implements CommandLineRunner {
-	
-	@Autowired
-	private FotoRepository fotoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeQualityApplication.class, args);
@@ -22,17 +13,6 @@ public class ChallengeQualityApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		Foto foto1 = new Foto(null, "-15.881257", "-48.084766" );
-		Foto foto2 = new Foto(null, "-111.22233", "-15.881257" );
-		Foto foto3 = new Foto(null, "-2.2322323", "-15.881257" );
-		Foto foto4 = new Foto(null, "-37.553343", "-15.881257" );
-		Foto foto5 = new Foto(null, "-44.111133", "-15.881257" );
-		Foto foto6 = new Foto(null, "-16.232323", "-15.881257" );
-		Foto foto7 = new Foto(null, "-12.55555", "-15.881257" );
-		Foto foto8 = new Foto(null, "-1212.1212", "-15.881257" );
-		
-		fotoRepository.save(Arrays.asList(foto1,foto2, foto3, foto4, foto5, foto6, foto7, foto8));			
 	}
 
 }
---
+++ src/main/java/com/lucasbarbosa/challenge/services/DBService.java
@@ -0,0 +1,30 @@
+package com.lucasbarbosa.challenge.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasbarbosa.challenge.domain.Foto;
+import com.lucasbarbosa.challenge.repositories.FotoRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private FotoRepository fotoRepository;
+	
+	public void instantiateTestDatabase() {
+		Foto foto1 = new Foto(null, "-15.881257", "-48.084766" );
+		Foto foto2 = new Foto(null, "-111.22233", "-15.881257" );
+		Foto foto3 = new Foto(null, "-2.2322323", "-15.881257" );
+		Foto foto4 = new Foto(null, "-37.553343", "-15.881257" );
+		Foto foto5 = new Foto(null, "-44.111133", "-15.881257" );
+		Foto foto6 = new Foto(null, "-16.232323", "-15.881257" );
+		Foto foto7 = new Foto(null, "-12.55555", "-15.881257" );
+		Foto foto8 = new Foto(null, "-1212.1212", "-15.881257" );
+		
+		fotoRepository.save(Arrays.asList(foto1,foto2, foto3, foto4, foto5, foto6, foto7, foto8));			
+		
+	}
+}
