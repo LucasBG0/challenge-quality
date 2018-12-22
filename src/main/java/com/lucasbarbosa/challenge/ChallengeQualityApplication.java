079eb10336a8cd0649938754d320f8ecff1ae9e1
Lucas Barbosa <lucasbarbosagomes0@gmail.com>
2018-12-22 12:04:21 -0200

    Initial Commit

---
+++ src/main/java/com/lucasbarbosa/challenge/ChallengeQualityApplication.java
@@ -0,0 +1,14 @@
+package com.lucasbarbosa.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeQualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeQualityApplication.class, args);
+	}
+
+}
+
