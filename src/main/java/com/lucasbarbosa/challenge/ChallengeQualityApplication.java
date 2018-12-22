f7f330d090d809ac045106eb10e8cd372df5382d
Lucas Barbosa <lucasbarbosagomes0@gmail.com>
2018-12-22 21:03:39 -0200

    Salvando o primeiro arquivo no S3

--- src/main/java/com/lucasbarbosa/challenge/ChallengeQualityApplication.java
+++ src/main/java/com/lucasbarbosa/challenge/ChallengeQualityApplication.java
@@ -1,11 +1,17 @@
 package com.lucasbarbosa.challenge;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucasbarbosa.challenge.services.S3Service;
+
 @SpringBootApplication
 public class ChallengeQualityApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeQualityApplication.class, args);
@@ -13,6 +19,7 @@ public class ChallengeQualityApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\fotos\\lana.jpg");		
 	}
 
 }
