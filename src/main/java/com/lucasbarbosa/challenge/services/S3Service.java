0dfe2bf734d6d5e87e88d5278f562842347bdad5
Lucas Barbosa <lucasbarbosagomes0@gmail.com>
2018-12-22 22:15:11 -0200

    Tratando excecoes das pictures

--- src/main/java/com/lucasbarbosa/challenge/services/S3Service.java
+++ src/main/java/com/lucasbarbosa/challenge/services/S3Service.java
@@ -5,8 +5,6 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import javax.management.RuntimeErrorException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.lucasbarbosa.challenge.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -35,7 +34,7 @@ public class S3Service {
 			String contentType = multipartFile.getContentType();
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Error de IO: " + e.getMessage());
+			throw new FileException("Error de IO: " + e.getMessage());
 		}
 	}
 
@@ -48,7 +47,7 @@ public class S3Service {
 			LOG.info("Upload finalizado!");
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
